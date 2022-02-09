82f0351ffde37a8e874a0001a1356c048da49d97
Василий Родин <varodg2000@mail.ru>
2022-02-09 16:41:18 +0300

    added json file, created medians

--- main.py
+++ main.py
@@ -3,14 +3,45 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import json
+from matplotlib import pyplot as plt
+from skimage.io import imread, imsave, imshow, show
+from skimage.filters import median
+from skimage.morphology import disk, diamond, star, rectangle
+import numpy as np
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+settings = {'parameter_0': 'image.jpg'
+            }
 
+# open file  and serialize settings in this file
+with open('settings.json', 'w') as fp:
+    json.dump(settings, fp)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+with open('settings.json') as info_data:
+    json_data = json.load(info_data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+path = json_data['parameter_0']
+image = imread(path)
+print(image.shape)
+imshow(image)
+
+median_1 = median(image, np.dstack(rectangle(8,6)), mode='wrap')
+median_2 = median(image, np.dstack((diamond(5),diamond(5),diamond(5))), mode='nearest')
+median_3 = median(image, np.dstack((disk(7),disk(7),disk(7))), mode='reflect')
+median_4 = median(image, np.dstack((star(3),star(3),star(3))), mode='mirror')
+median_5 = median(image, np.dstack((disk(7),disk(7),disk(7))), mode='mirror')
+
+fig, axes=plt.subplots(nrows=2, ncols=3,figsize=( 15, 5))
+axes[0][0].set_axis_off()
+axes[0][0].imshow(image)
+axes[0][1].set_axis_off()
+axes[0][1].imshow(median_1)
+axes[0][2].set_axis_off()
+axes[0][2].imshow(median_2)
+axes[1][0].set_axis_off()
+axes[1][0].imshow(median_3)
+axes[1][1].set_axis_off()
+axes[1][1].imshow(median_4)
+axes[1][2].set_axis_off()
+axes[1][2].imshow(median_4)
+show()
